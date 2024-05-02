accef77830634700ba5c37593e2dfd2517890935
KRegalado <kimberlyregalado0924@gmail.com>
2024-05-01 20:44:06 -0700

    FlashLight Moves, no restrictions yet

--- GDIM 61 Project/Assets/Scipts/Player Scripts/PlayerLight.cs
+++ GDIM 61 Project/Assets/Scipts/Player Scripts/PlayerLight.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class PlayerLight : MonoBehaviour
 {
     [SerializeField] private GameObject m_flashLight;
+    private Transform m_lightTransform;
+    public GameObject m_player;
+
+    private void Awake()
+    {
+
+        m_lightTransform = transform.Find("Arm");
+    }
 
     private void Start()
     {
@@ -25,5 +34,13 @@ public class PlayerLight : MonoBehaviour
                 m_flashLight.SetActive(false);
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float Z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        m_lightTransform.eulerAngles = new Vector3 (0f, 0f, Z);
     }
 }
