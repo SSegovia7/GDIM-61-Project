05981bb617260b0a3b91b837150674c93fab932d
KRegalado <kimberlyregalado0924@gmail.com>
2024-05-03 09:06:24 -0700

    Character Flip
    There is a bug with the flashlight when you are facing to the left

--- GDIM 61 Project/Assets/Scipts/Player Scripts/PlayerMove.cs
+++ GDIM 61 Project/Assets/Scipts/Player Scripts/PlayerMove.cs
@@ -7,12 +7,34 @@ public class PlayerMove : MonoBehaviour
     //Getting and setting player movement
     [SerializeField] private Rigidbody2D m_rb;
     [SerializeField] private float m_speed;
-    private float moveForce;
+    private float inputHorizontal;
+    public bool facingRight = true;
 
-    private void Update()
+    private void FixedUpdate()
     {
         //Character moves side to side
-        moveForce = Input.GetAxis("Horizontal");
-        m_rb.velocity = new Vector2(moveForce * m_speed, m_rb.velocity.y);
+        inputHorizontal = Input.GetAxis("Horizontal");
+        m_rb.velocity = new Vector2(inputHorizontal * m_speed, m_rb.velocity.y);
+        
+        //Flipping Character
+        if (inputHorizontal > 0 && !facingRight)
+        {
+            Flip();
+        }
+
+        if (inputHorizontal < 0 && facingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    private void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
     }
 }
