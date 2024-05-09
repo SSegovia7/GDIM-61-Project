34b5c40c3941a02d9e54259ad310140f421620e9
KRegalado <kimberlyregalado0924@gmail.com>
2024-05-08 22:21:53 -0700

    Added a FlashLight tag

--- GDIM 61 Project/Assets/Scipts/Mobs/Mob.cs
+++ GDIM 61 Project/Assets/Scipts/Mobs/Mob.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Mob : MonoBehaviour
 {
+    [SerializeField] private GameObject m_mob;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,21 @@ public class Mob : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.CompareTag("FlashLight"))
+        {
+            //
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("FlashLight"))
+        {
+            //
+        }
     }
 }
